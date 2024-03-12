5d956d632a8851be186db99d78c6085ed0b47bd7
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-03-13 03:55:15 +0700

    add nested prop for demo

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -15,6 +15,7 @@ namespace demo_azure_deployment.Pages
         public string? AppVal1 { get; set; }
         public string? AppVal2 { get; set; }
         public string? AppVal3 { get; set; }
+        public string? PropNested { get; set; }
 
         public void OnGet()
         {
@@ -22,7 +23,7 @@ namespace demo_azure_deployment.Pages
             AppVal1 = config["AppVal1"];
             AppVal2 = config.GetSection("AppVal2").Value;
             AppVal3 = config.GetValue<string>("AppVal3");
-
+            PropNested = config["AppVal4:PropNested"];
         }
     }
 }
